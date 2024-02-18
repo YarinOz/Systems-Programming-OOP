ce3015e297460280319252078947551c4f21d36c
YarinOz <yoyomoder@gmail.com>
2024-02-18 10:10:57 +0200

    new assignment

---
+++ task03/MyQueue.h
@@ -0,0 +1,5 @@
+#ifndef __MYQUEUE_H
+#define __MYQUEUE_H
+
+
+#endif //__MYQUEUE_H
---
+++ task03/StackNode.cpp
@@ -0,0 +1,4 @@
+/* Assignment C++: 1
+   Author: Yarin Oziel, ID: 319149878
+   Author: Itamar Meir, ID: 208536888
+*/ 
