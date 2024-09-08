aab2d33910c05e650987ac9febf8af77c9c9d193
Yarin Oziel <139881999+YarinOz@users.noreply.github.com>
2024-09-08 12:07:53 +0300

    Update CheckingAccount.cpp

--- task04/CheckingAccount.cpp
+++ task04/CheckingAccount.cpp
@@ -1,8 +1,3 @@
-/* Assignment C++: 2
-   Author: Yarin Oziel, ID: 319149878
-   Author: Itamar Meir, ID: 208536888
-*/ 
-
 #include "CheckingAccount.h"
 
 CheckingAccount::CheckingAccount(const std::string& number, const std::string& holder, const double initialBalance, const double overdraft)
