ef3fa2168ea89d38cff4725a22a40983c86ed442
Yarin Oziel <139881999+YarinOz@users.noreply.github.com>
2024-09-08 12:08:20 +0300

    Update Account.cpp

--- task04/Account.cpp
+++ task04/Account.cpp
@@ -1,8 +1,3 @@
-/* Assignment C++: 2
-   Author: Yarin Oziel, ID: 319149878
-   Author: Itamar Meir, ID: 208536888
-*/ 
-
 #include "Account.h"
 
 Account::Account(const std::string& number, const std::string& holder, double initialBalance)
